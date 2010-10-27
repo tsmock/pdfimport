d4761fbb093c69709d776caa82f1a4a51e36212d
Viesturs Zariņš <viesturz@gmail.com>
2010-10-27 13:38:28 +0000

    Transition to pdfbox library.

--- src/pdfimport/PdfImportAction.java
+++ src/pdfimport/PdfImportAction.java
@@ -3,29 +3,10 @@ package pdfimport;
 
 import static org.openstreetmap.josm.tools.I18n.tr;
 
-import it.stefanochizzolini.clown.documents.Document;
-import it.stefanochizzolini.clown.documents.Page;
-import it.stefanochizzolini.clown.documents.contents.objects.ContentObject;
-import it.stefanochizzolini.clown.files.File;
-import it.stefanochizzolini.clown.tokens.FileFormatException;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
-import java.io.FileNotFoundException;
-
-import javax.swing.JFileChooser;
-import javax.swing.JOptionPane;
-import javax.swing.SwingUtilities;
-import javax.swing.filechooser.FileFilter;
-
-import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.actions.JosmAction;
-import org.openstreetmap.josm.command.Command;
-import org.openstreetmap.josm.data.osm.DataSet;
-import org.openstreetmap.josm.gui.layer.OsmDataLayer;
-
-
 import org.openstreetmap.josm.tools.Shortcut;
 
 /**
@@ -39,7 +20,7 @@ public class PdfImportAction extends JosmAction {
                 tr("Import PDF file."), Shortcut.registerShortcut(
                         "tools:pdfimport", tr("Tool: {0}",
                                 tr("Import PDF file")), KeyEvent.VK_P,
-                        Shortcut.GROUP_EDIT, Shortcut.SHIFT_DEFAULT), true);
+                                Shortcut.GROUP_EDIT, Shortcut.SHIFT_DEFAULT), true);
     }
 
     /**
@@ -49,7 +30,7 @@ public class PdfImportAction extends JosmAction {
      *            Action Event
      */
     public void actionPerformed(ActionEvent e) {
-        
+
         //show dialog asking to select coordinate axes and input coordinates and projection.
         LoadPdfDialog dialog = new LoadPdfDialog();
         dialog.setAlwaysOnTop(true);
